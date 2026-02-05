70db31caf9b4ee4d8bddb0d3d673c5b983c2dc6b
Han Yue <alexyuehan@gmail.com>
2026-02-05 01:37:43 -0500

    Initial Commit

---
+++ Codenames/CodenamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodenamesApp.swift
+//  Codenames
+//
+//  Created by Frostmourne on 2026-02-05.
+//
+
+import SwiftUI
+
+@main
+struct CodenamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codenames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Codenames
+//
+//  Created by Frostmourne on 2026-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
